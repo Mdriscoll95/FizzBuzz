2a942c2525b14c1c5df09ee0ba72705a17fc1289
Michael Driscoll <michael.driscoll951@gmail.com>
2023-06-21 19:19:43 -0700

    hw

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿static void FizzBuzz(int num)
+{
+    if (num % 3 == 0 && num % 5 != 0)
+    {
+        Console.WriteLine("fizz");
+    }
+    else if (num % 5 == 0 && num % 3 != 0)
+    {
+        Console.WriteLine("buzz");
+    }
+    else if (num % 5 == 0 && num % 3 == 0)
+    {
+        Console.WriteLine("fizzbuzz");
+    }
+    else
+    {
+        Console.WriteLine(num);
+    }
+}
+
+
+
+
